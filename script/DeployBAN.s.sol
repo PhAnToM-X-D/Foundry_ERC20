2709c1deddbe1ecc723681c6ee909e47316e6fd5
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-01 16:57:00 +0530

    Written the deploy script for the BAN token

---
+++ script/DeployBAN.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {BaniaToken} from "../src/BaniaToken.sol";
+
+contract DeployBAN is Script {
+    uint256 immutable i_init;
+
+    constructor(uint256 init) {
+        i_init = init;
+    }
+
+    function run() external returns (BaniaToken) {
+        vm.startBroadcast();
+        BaniaToken ban = new BaniaToken(i_init);
+        vm.stopBroadcast();
+        return ban;
+    }
+}
