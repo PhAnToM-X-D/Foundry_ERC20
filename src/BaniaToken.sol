c47ddd2fe3c623fe133ec1ff28ec0d0f862bfabd
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-01 16:47:44 +0530

    written the code for BaniaTokens

---
+++ src/BaniaToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC20} from "lib/openzepplin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract BaniaToken is ERC20 {
+    constructor(uint256 _initialSupply) ERC20("Bania Token", "BAN") {
+        _mint(msg.sender, _initialSupply ** decimals());
+    }  
+}
